60d749728878891bfc1cc92d802a0918a1b4c1f2
elCuervo <yo@brunoaguirre.com>
2014-10-15 22:32:34 -0200

    Create tables

--- lib/spirit.rb
+++ lib/spirit.rb
@@ -1,29 +1,65 @@
-require 'pg'
+require 'sequel'
+
+Sequel.extension :pg_hstore, :pg_hstore_ops
 
 module Spirit
-  def self.connect
+  MissingID = Class.new(StandardError)
+
+  def self.connect(url = ENV['DATABASE_URL'])
+    @_url = url
   end
 
-  def self.connected?
-    true
+  def self.database_url
+    @_url ||= ENV['DATABASE_URL']
+  end
+
+  def self.connection
+    @_connection ||= Sequel.connect(database_url)
   end
 
   class Model
     def initialize(attrs = {})
-      @attributes = {}
+      @attributes = Sequel.hstore({})
       update_attributes(attrs)
     end
 
     def id
-      1
+      raise MissingID if !defined?(@_id)
+      @_id
     end
 
     def update_attributes(attrs)
       attrs.each { |name, value| send(:"#{name}=", value) }
     end
 
+    def table_name
+      self.class.name.to_sym
+    end
+
+    def conn
+      Spirit.connection
+    end
+
+    def save
+      conn.execute("CREATE EXTENSION IF NOT EXISTS hstore")
+      conn.create_table? table_name do
+        primary_key :id
+        hstore      :data
+      end
+
+      table = conn[table_name]
+
+      if defined? @_id
+        table.update data: @attributes
+      else
+        @_id = table.insert data: @attributes
+      end
+
+      self
+    end
+
     def self.create(attrs = {})
-      new(attrs)
+      new(attrs).save
     end
 
     def self.attribute(name)
--- test/spirit_test.rb
+++ test/spirit_test.rb
@@ -1,23 +1,35 @@
 require 'cutest'
 require 'spirit'
 
-test 'connect' do
+setup do
   Spirit.connect
-  assert Spirit.connected?
+end
+
+Model = Class.new(Spirit::Model)
+
+class User < Spirit::Model
+  attribute :name
 end
 
 test 'model' do
-  Model = Class.new(Spirit::Model)
-
   model = Model.create
-  assert model.id
+  assert_equal model.id.class, Fixnum
 end
 
 test 'attributes' do
-  class User < Spirit::Model
-    attribute :name
-  end
-
   user = User.create name: 'Michel'
   assert_equal user.name, 'Michel'
+end
+
+test 'update' do
+  user = User.create name: 'Ciril'
+  id = user.id
+
+  assert_equal user.name, 'Ciril'
+
+  user.name = 'cyx'
+  user.save
+
+  assert_equal user.name, 'cyx'
+  assert_equal user.id, id
 end
